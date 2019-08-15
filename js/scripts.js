696cb6a258d14e14f850e458ea20360aa5be3347
dominic montelongo and nikhil batta <nikhil@>
2019-08-15 10:23:00 -0700

    add functionality to buttons and hides and shows with branching

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,17 @@ $(document).ready(function(){
     var age = parseInt($("#userInput1").val());
     console.log(age)
     if (age<18){
-      $("#formOne").append("<h1> Please try again when youre 18 :( </h1>")
+  $(".okok").show();
+
     }
     else {
       $(".hidden").show()
+      $(".okok").hide();
     }
   });
   $("#formTwo").submit(function(event){
     event.preventDefault();
     var checked = $("input:radio[name=wang]:checked").val();
-    // var userJoe = $("input:radio[name=wang]:checked").val();
-    // var userJose = $("input:radio[name=wang]:checked").val();
-    // var userWang = $("input:radio[name=wang]:checked").val();
     $("#formTwo").append("<h1>" + checked + "</h1>")
   });
 });
